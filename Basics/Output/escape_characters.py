4d3c3406c17bd6977d9226ccd88ddd5fb73399ea
repl.it user <replituser@example.com>
2021-02-26 11:27:26 +0000

    Have added in the escape characters

---
+++ Basics/Output/escape_characters.py
@@ -0,0 +1,7 @@
+#These are the example of the escape characters that help edit your printing. 
+
+print("\n Display a new line")
+print("\t Displays a tab Space")
+print("\\ Displays a backlash")
+print("\" Displays a double quote")
+print("\' Display single quote")
