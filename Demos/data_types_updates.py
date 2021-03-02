34d73a873bfef09b7f1bcac6da50a80adfc05ac1
repl.it user <replituser@example.com>
2021-03-02 10:41:14 +0000

    Done the coding using if and else.

---
+++ Demos/data_types_updates.py
@@ -0,0 +1,9 @@
+print("Whats is your name?")
+name = input()
+print("What is your age?")
+age = int(input())
+print("What is you bank balance?")
+bal = float(input())
+
+# Practice the new set up of output below
+print("Welcome {}. You are said to be {} years old. Your bank balance is {}." .format(name, age, bal))
