d64e73d5e407f0a7f5e829228ccce2b975ab70a4
repl.it user <replituser@example.com>
2021-03-12 10:05:14 +0000

    Code to demonstrate simple loops

---
+++ Basics/repetitions/while_loop/simple.py
@@ -0,0 +1,10 @@
+print("How many cables should I remove?")
+cables = int(input())
+
+cables_removed = 0
+
+print()
+
+while (cables_removed < cables):
+    print("Removed cable.")
+    cables_removed = cables_removed + 1
