6abd5cf15aa73cd1080a188e0b00b524b8482224
A0M0305 <alberto.orlando@hotmail.co.uk>
2021-03-26 10:09:10 +0000

    Piotr example of a tinder app... matching.

---
+++ Demos/LeTinder.py
@@ -0,0 +1,23 @@
+def interest():
+    print("Enter your interest, one after the other, and enter \"STOP\" to end.")
+    set1 = set()
+    activity = ""
+    while activity != "stop":
+        activity = input()
+        set1.add(activity)
+    return set1
+
+
+def tinderTheSecond():
+    print("First Person: ")
+    P1set = interest()
+    print("Second Person: ")
+    P2set = interest()
+    common = P1set.intersection(P2set)
+    if len(common) > 4:
+        print(f"Yay - you are a match! you have {len(common)} interest in common!")
+    else:
+        print(f"Well, don't think it will work out :(, you have {len(common)} interest in common")
+
+
+tinderTheSecond()
