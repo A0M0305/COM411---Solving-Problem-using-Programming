dfaf632dd3144522921f3cb84d5a54b7b646b5a9
A0M0305 <alberto.orlando@hotmail.co.uk>
2021-04-30 09:46:44 +0100

    Plot examples with Piotr

---
+++ Demos/plot_displayPlotting.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+
+fig, axes = plt.subplots(2,2)
+
+x = ["Poland", "Romania", "Bangladesh"]
+y = [2, 17, 2]
+y2 = [5, 6, 12]
+y3 = [1, 1, 3]
+
+axes[0,0].plot(x,y)
+axes[0,1].bar(x,y2)
+axes[1,1].step(x,y3)
+
+plt.show()
