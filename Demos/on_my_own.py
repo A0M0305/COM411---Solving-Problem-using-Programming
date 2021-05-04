e7c1663b7899fd9ba836819a7f055c1a1173a259
A0M0305 <alberto.orlando@hotmail.co.uk>
2021-05-04 11:01:12 +0100

    code to display simple subplots

---
+++ Demos/on_my_own.py
@@ -0,0 +1,19 @@
+def orlando():
+    print("He is going to be 5 Years old")
+    print("he is nuts")
+
+
+orlando()
+
+
+def mia(age):
+    print("Mis is currently 3")
+    return age + 1
+
+
+mia_bday = mia(3)
+
+print("but she will be {}.".format(mia_bday))
+
+
+
